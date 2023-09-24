0b7f388d713fbb8c329f484f997ec6e8df723941
Ruan Aragão <ruanaragao2@gmail.com>
2023-09-23 21:12:03 -0300

    🐞 fix: fix typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Admin Dashbaord',
+  title: 'Admin Dashboard',
   description: 'Admin Dashboard',
 }
 
